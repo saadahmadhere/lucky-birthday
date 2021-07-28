792145f952446953107c45f93ab5688445d3eebd
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-07-28 22:01:26 +0530

    complete top section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import bg from './bg.svg'
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+      <div className="top-section">
+           <div className="pageTitle">
+               <h1>Is Your Birthday Lucky?</h1>
+               <a href="#mainSection">scroll down to checkout</a>
+            </div>
+            <div className="bottom-section">
+               <h1>Enter Your Birthdate and lucky number to continue...</h1>
+            </div>
+      </div>
     </div>
   );
 }
